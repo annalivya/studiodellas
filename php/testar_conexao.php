fb0b4382364c16e2a7e8ace6406efc277acb2e31
annalivya <annalivyalunamarques@gmail.com>
2024-11-21 13:00:38 -0300

    Delete testar_conexao.php

--- php/testar_conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conexao = new mysqli('localhost', 'root', '', 'studio_dellas');
-
-if ($conexao->connect_error) {
-    die("Falha na conexão: " . $conexao->connect_error);
-} else {
-    echo "Conexão com o banco de dados bem-sucedida!";
-}
-?>
